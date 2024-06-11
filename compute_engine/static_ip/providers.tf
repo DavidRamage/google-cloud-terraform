c2dfcb16f622900c9972584034811c3df7b6e99e
David Ramage <david.p.ramage@gmail.com>
2024-06-11 09:41:45 -0700

    remove commented out line

--- compute_engine/static_ip/providers.tf
+++ compute_engine/static_ip/providers.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  //project = "gentle-post-425320-d0"
   project     = var.gcloud_project
   region      = "us-west1"
   credentials = file("~/credentials/gcloud-default-tf-creds.json")
